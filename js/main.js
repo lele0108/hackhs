ddb5689109522628cb44bad1947fd3b8313afb59
Jimmy Liu <sicong.liu98@gmail.com>
2013-12-23 18:05:02 -0800

    new bg

--- js/main.js
+++
@@ -1,5 +0,0 @@
-var tswitch = document.getElementById('switch');
-tswitch.onclick = function() {
-       this.style.display = 'none';
-       document.getElementById('lights').className = 'animate';
-};
